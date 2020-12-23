9c4ae2ee2ff2a6dab7745aa32996c1b2a0baf269
François de Mareschal <francois.de-mareschal@outlook.fr>
2020-12-24 00:15:19 +0100

    Make main() to call the run() from library to actually perform calculations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process;
+use structopt::StructOpt;
+use yatti::Config;
+
 fn main() {
-    println!("Hello, world!");
+    // Parse provided arguments.
+    let options = Config::from_args();
+
+    println!("Hit any key (but q) in cadence (q to quit).");
+
+    // Run the tempo calculator.
+    if let Err(e) = yatti::run(options) {
+        eprintln!("[ERROR] {}", e);
+        process::exit(1);
+    }
+
+    println!("Goodbye!");
 }
